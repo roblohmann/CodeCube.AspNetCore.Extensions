2e0c41b0732e96c4ff45ad9047d73d40ff229670
Rob Lohmann <roblohmann@hotmail.com>
2021-12-01 10:26:28 +0100

    update readme and bit of refactoring

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # CodeCube.AspNetCore.Extensions
 This library provides extensions for the .NET Core and .NET 5 framework in general
+
+#Examples
+
+##Example 1 - Default settings
+
+When using the default implementation, the version of the current entry assembly is taken to return in a header with the name 'x-application-version'
+
+```C#
+app.UseVersionHeader();
+```
+
+##Example 2 - Custom settings
+
+You can set the assembly to grab the version from, the header name or even the version yourself.
+
+```C#
+app.UseVersionHeader(options =>
+            {
+                options.Assembly = "CodeCube.AspnetCore.Extensions";
+                options.HeaderName = "x-my-versionheader";
+                options.Version = "1.0.0";
+            });
+```
--- src/CodeCube.AspNetCore.Extensions/AddHeaderExtensions.cs
+++ src/CodeCube.AspNetCore.Extensions/AddHeaderExtensions.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Reflection;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Http;
 
 namespace CodeCube.AspNetCore.Extensions
 {
@@ -19,52 +16,6 @@ namespace CodeCube.AspNetCore.Extensions
             setupAction?.Invoke(options);
 
             return builder.UseMiddleware<AddHeadersMiddleware>(options);
-        }
-    }
-
-    public class AddHeadersMiddleware
-    {
-        private readonly RequestDelegate _next;
-        private readonly VersionHeaderOptions _options;
-
-        //The middleware class must include:
-        //1. A public constructor with a parameter of type RequestDelegate.
-        public AddHeadersMiddleware(RequestDelegate next, VersionHeaderOptions options)
-        {
-            _next = next;
-            _options = options ?? new VersionHeaderOptions();
-        }
-
-        //2. A public method named Invoke or InvokeAsync.This method must:
-        //   - Return a Task.
-        //   - Accept a first parameter of type HttpContext.
-        public async Task InvokeAsync(HttpContext context)
-        {
-            var versionForHeader = GetVersion(_options.Version, _options.Assembly);
-
-            context.Response.OnStarting((state) =>
-            {
-                context.Response.Headers.Add(_options.HeaderName, versionForHeader);
-
-                return Task.FromResult(0);
-            }, context);
-
-            await _next.Invoke(context);
-        }
-
-        private static string GetVersion(string optionsVersion, string optionsAssembly)
-        {
-            if (!string.IsNullOrWhiteSpace(optionsVersion)) return optionsVersion;
-
-            var theAssembly = !string.IsNullOrWhiteSpace(optionsAssembly) ? Assembly.Load(optionsAssembly) : Assembly.GetEntryAssembly();
-            var assemblyVersionInformation = theAssembly?.GetCustomAttribute<AssemblyFileVersionAttribute>();
-
-            if (assemblyVersionInformation != null)
-            {
-                return assemblyVersionInformation.Version;
-            }
-
-            return "0.0.0.0";
         }
     }
 }
